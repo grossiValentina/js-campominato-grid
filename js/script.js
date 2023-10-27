e5f14e845096669ebb80a24b5686b3939703d0c9
grossiValentina <grossivalentina435@gmail.com>
2023-10-27 19:17:55 +0200

    add: exercise

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// prelevo elemento html
+const gridElem = document.querySelector(".grid");
+
+// ciclo for 
+
+ for (let i = 1; i < 100; i++) {
+        console.log(i)
+        const cell = generateGridCell(i + 1)
+        cell.addEventListener("click", eventClick)
+        gridElem.appendChild(cell)
+       
+}
+
+
+
+// FUNZIONI 
+
+// 
+function generateGridCell(cellNumber) {
+    const cellElem = document.createElement("div")
+    cellElem.classList.add("cell")
+    cellElem.textContent = cellNumber
+   
+    return cellElem;
+}
+
+function eventClick() {
+    const clickNumber = parseInt(this.textContent)
+        this.classList.add("pink")
+        
+    
+}
+
